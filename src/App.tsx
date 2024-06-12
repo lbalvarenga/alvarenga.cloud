58879ed5695fa6490e958f5533ccbe3eb61ecf8a
Lucas Alvarenga <alvarengalucas55@gmail.com>
2024-06-12 13:22:51 -0300

    Initial Commit

---
+++ src/App.tsx
@@ -0,0 +1,26 @@
+import "./App.css";
+import { MyPdfViewer } from "./pdf";
+
+function App() {
+  return (
+    <>
+      <img src="/favicon.png" width="180" id="icon" />
+      <h1>Lucas Alvarenga</h1>
+      <h2>Cloud Architect; DevOps; Fullstack</h2>
+      <div id="links">
+        <a
+          href="https://www.linkedin.com/in/lal-lucas-alvarenga/"
+          target="_blank"
+        >
+          LinkedIn
+        </a>
+        <a href="https://www.github.com/lbalvarenga/" target="_blank">
+          GitHub
+        </a>
+      </div>
+      <MyPdfViewer />
+    </>
+  );
+}
+
+export default App;
---
+++ src/pdf.tsx
@@ -0,0 +1,51 @@
+import React, { useState, useRef } from "react";
+import { usePdf } from "@mikecousins/react-pdf";
+
+export const MyPdfViewer = () => {
+  const [page, setPage] = useState(1);
+  const canvasRef = useRef(null);
+
+  const { pdfDocument, pdfPage } = usePdf({
+    file: "/cv.pdf",
+    page,
+    canvasRef,
+  });
+
+  return (
+    <div>
+      {!pdfDocument && <span></span>}
+      <canvas ref={canvasRef} id="pdf" />
+      {Boolean(pdfDocument && pdfDocument.numPages) && (
+        <nav id="pdfnav">
+          <ul className="pager">
+            {Boolean(pdfDocument && pdfDocument.numPages > 1) && (
+              <>
+                <li className="previous">
+                  <button
+                    disabled={page === 1}
+                    onClick={() => setPage(page - 1)}
+                  >
+                    Previous
+                  </button>
+                </li>
+                <li className="next">
+                  <button
+                    disabled={page === pdfDocument!.numPages}
+                    onClick={() => setPage(page + 1)}
+                  >
+                    Next
+                  </button>
+                </li>
+              </>
+            )}
+            <li>
+              <a href="/cv.pdf" target="_blank">
+                <button>Download</button>
+              </a>
+            </li>
+          </ul>
+        </nav>
+      )}
+    </div>
+  );
+};
